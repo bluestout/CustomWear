169d722d6833dd89a29bd3e8294ee4d2df131355
ecomdev-star <ecomdevstar0116@gmail.com>
2025-07-08 15:42:30 -0400

    Clip Art

--- assets/es.js
+++ assets/es.js
@@ -19,3 +19,41 @@ const observer = new MutationObserver((mutations, obs) => {
 });
 
 observer.observe(document.body, { childList: true, subtree: true });
+
+const initESSlider = element => {
+  const sw = new Swiper(element, {
+    slidesPerView: 4,
+    slidesPerGroup: 4,
+    grid: {
+      rows: 8,
+    },
+    spaceBetween: 10,
+    navigation: {
+      nextEl: element?.querySelector('.swiper-button-next'),
+      prevEl: element?.querySelector('.swiper-button-prev'),
+    },
+    pagination: {
+      el: element?.querySelector('.swiper-pagination'),
+      clickable: true,
+      renderBullet: (index, className) => {
+        return '<span class="' + className + '">' + (index + 1) + '</span>';
+      },
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        slidesPerGroup: 2,
+        grid: {
+          rows: 8,
+        },
+      },
+      769: {
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        grid: {
+          rows: 8,
+        },
+      },
+    },
+  });
+}
